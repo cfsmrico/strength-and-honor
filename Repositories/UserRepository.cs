24277fc4fd5fde4156651d96695a5ebef3896fb2
Chaney Fonseca <chaneyfonseca@gmail.com>
2020-06-29 21:30:15 -0500

    add a MongoDRepository

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StrengthAndHonor.Services
+{
+    public class UserRepository : IUserRepository
+    {
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using StrengthAndHonor.Settings;
 
 namespace StrengthAndHonor
 {
@@ -20,6 +22,11 @@ namespace StrengthAndHonor
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<MongoDbSettings>(Configuration.GetSection("MongoDbSettings"));
+
+            services.AddSingleton<IMongoDbSettings>(serviceProvider =>
+                serviceProvider.GetRequiredService<IOptions<MongoDbSettings>>().Value);
+
             services.AddControllersWithViews();
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
